4ff5e1d7edb6732edf5857bb41ef63aab7e90157
Levon Baghdasaryan <levonbaghdasarian@gmail.com>
2021-07-28 23:18:46 +0000

    Exporting CSV

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Http\Resources\OrderResource;
 use App\Order;
-use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Response;
 
 class OrderController extends Controller
 {
@@ -18,5 +18,34 @@ class OrderController extends Controller
     public function show($id)
     {
         return new OrderResource(Order::find($id));
+    }
+
+    public function export()
+    {
+        $headers = [
+            "Content-type" => "text/csv",
+            "Content-Disposition" => "attachment; filename=orders.csv",
+            "Pragma" => "no-cache",
+            "Cache-Control" => "must-revalidate, post-check=0, pre-check=0",
+            "Expires" > "0"
+        ];
+
+        return Response::stream(function () {
+            $orders = Order::all();
+            $file = fopen('php://output', 'w');
+
+            //Header Row
+            fputcsv($file, ['ID', 'Name', 'Email', 'Product Title', 'Price', 'Quantity']);
+
+            //Body
+            foreach ($orders as $order) {
+                fputcsv($file, [$order->id, $order->name, $order->email, '', '', '']);
+                foreach ($order->orderItems as $orderItem) {
+                    fputcsv($file, ['', '', '', $orderItem->product_title, $orderItem->price, $orderItem->quantity]);
+                }
+            }
+
+            fclose($file);
+        }, 200, $headers);
     }
 }
--- app/Order.php
+++ app/Order.php
@@ -6,15 +6,20 @@ use Illuminate\Database\Eloquent\Model;
 
 class Order extends Model
 {
-   public function orderItems()
-   {
-       return $this->hasMany(OrderItem::class);
-   }
+    public function orderItems()
+    {
+        return $this->hasMany(OrderItem::class);
+    }
 
-   public function getTotalAttribute()
-   {
-       return $this->orderItems->sum(function(OrderItem $item) {
+    public function getTotalAttribute()
+    {
+        return $this->orderItems->sum(function (OrderItem $item) {
             return $item->price * $item->quantity;
-       });
-   }
+        });
+    }
+
+    public function getNameAttribute()
+    {
+        return $this->first_name . ' ' . $this->last_name;
+    }
 }
